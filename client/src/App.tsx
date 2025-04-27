0f14a61be7a18a31f5a06993eed68ccad53f10bc
Radha Kotra <kradha6113@gmail.com>
2025-04-27 13:56:41 -0400

    Create main app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,32 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import PrivateRoute from './components/common/PrivateRoute'
+import LoginPage from '/pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import DashboardPage from './pages/DashboardPage'
+import GoalsPage from './pages/GoalsPage'
+import ExpensesPage from './pages/ExpensesPage'
+import NavBar from './component/common/Navbar'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <AuthProvider>
+        <NavBar />
+        <div className="container mt-4">
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
+            <Route path="/goals" element={<PrivateRoute><GoalsPage /></PrivateRoute>} />
+            <Route path="/expenses" element={<PrivateRoute><ExpensesPage /></PrivateRoute>} />
+          </Routes>
+        </div>
+      </AuthProvider>
+    </Router>
   )
 }
 
