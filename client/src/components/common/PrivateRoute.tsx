6d359a16f2d3e68051056b8b2678b492f69b0a2f
Radha Kotra <kradha6113@gmail.com>
2025-04-27 14:06:16 -0400

    Create PrivateRoute component

---
+++ client/src/components/common/PrivateRoute.tsx
@@ -0,0 +1,18 @@
+import { Navigate } from 'react-router-dom'
+import { useAuth } from '../../context/AuthContext'
+
+const PrivateRoute = ({ children }: { children: JSX.Element }) => {
+    const { user, loading } = useAuth()
+
+    if (loading) {
+        return <div className="d-flex justify-content-center mt-5">
+            <div className='spinner-border' role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    }
+
+    return user ? children : <Navigate to="/login" />
+}
+
+export default PrivateRoute
