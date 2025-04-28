6de2f89dd0bf9aee565b4e08ac389da33b95e48c
Radha Kotra <kradha6113@gmail.com>
2025-04-27 20:53:07 -0400

    Add PrivateRoute component

---
+++ client/src/components/common/Navbar.tsx
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom'
+import { useAuth } from '../../conext/AuthContext'
+
+const Navbar = () => {
+    const { user, logout } = useAuth()
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className='container'>
+                <Link className="navbar-brand" to="/">Prophecy</Link>
+                <button className='navbar-toggler' type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span className='navbar-toggler-icon'></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav me-auto">
+                        {user && (
+                            <>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/">Dashboard</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/goals">Goals</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/expenses">Expenses</Link>
+                                </li>
+                            </>
+                        )}
+                    </ul>
+                    <ul className='navbar-nav'>
+                        {user ? (
+                            <li className='nav-item'>
+                                <button className="nav-link btn"
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
--- client/src/components/common/PrivateRoute.tsx
+++ client/src/components/common/PrivateRoute.tsx
@@ -1,5 +1,6 @@
 import { Navigate } from 'react-router-dom'
 import { useAuth } from '../../context/AuthContext'
+import { JSX } from 'react'
 
 const PrivateRoute = ({ children }: { children: JSX.Element }) => {
     const { user, loading } = useAuth()
