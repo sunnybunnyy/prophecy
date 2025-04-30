54ca582f660bcb4abcad093dd4d43b278c9e4a26
Radha Kotra <kradha6113@gmail.com>
2025-04-29 22:49:28 -0400

    Add Expense type definition

---
+++ client/src/types/index.ts
@@ -0,0 +1,11 @@
+export type Expense = {
+    id: number;
+    name: string;
+    amount: number;
+    type: string;
+    frequency: string;
+    date: string;
+    userId: number;
+    createdAt: string;
+    updatedAt: string;
+};
